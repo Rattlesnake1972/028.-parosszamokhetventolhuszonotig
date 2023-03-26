12bea1c0ce57712f21359fd71a11847eea10b7df
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 16:56:29 +0200

    70-től 25-ig a páros számok

---
+++ 028. parosszamokhetventolhuszonotig/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _028.parosszamokhetventolhuszonotig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 70; i > 24; i--)
+            {
+                if (i % 2 == 0)
+
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
